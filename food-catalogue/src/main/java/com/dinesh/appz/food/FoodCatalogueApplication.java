62527f56768b611bf2ea58dfac8081b34983a2e8
Dinesh Kumar Bhardwaj <dinesh.bhardwaj1710@gmail.com>
2023-09-25 21:27:21 +0200

    Food Catalogue Service - Initial Commit.

---
+++ food-catalogue/src/main/java/com/dinesh/appz/food/FoodCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.appz.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCatalogueApplication.class, args);
+	}
+
+}
---
+++ food-catalogue/src/test/java/com/dinesh/appz/food/FoodCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.appz.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
