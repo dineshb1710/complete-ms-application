e3a1d9572cf68f0103cc7f1431376f86ccf62fc0
Dinesh Kumar Bhardwaj <dinesh.bhardwaj1710@gmail.com>
2023-09-25 19:56:01 +0200

    User Information Service - Initial Commit

---
+++ user-info/src/test/java/com/dinesh/appz/user/UserInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.appz.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
